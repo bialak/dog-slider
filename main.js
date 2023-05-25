afe58d8ac3583219729f3396419b66678b2c772c
Klaudia Biala <klaudiabiaa@gmail.com>
2023-05-25 16:18:14 +0200

    Adding Paws to pictures

--- main.js
+++ main.js
@@ -5,15 +5,22 @@ const slideList = [
 	"images/img4.jpg",
 	"images/img5.jpg",
 ];
-
+const paws = [...document.querySelectorAll(".paw")];
 const dogImage = document.querySelector(".photo");
 let slideIndex = 0;
+
+const changePaw = () => {
+	document.querySelector(".active").classList.remove("active");
+	paws[slideIndex].classList.add("active");
+};
 
 function showSlides() {
 	slideIndex++;
 	if (slideIndex + 1 > slideList.length) {
 		slideIndex = 0;
 	}
+	changePaw();
+
 	dogImage.src = slideList[slideIndex];
 }
 
